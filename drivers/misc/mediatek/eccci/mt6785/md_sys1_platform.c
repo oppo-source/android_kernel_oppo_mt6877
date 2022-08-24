b28359e7f304403ae27915c5ada649898f1c758b
Weiqiang Hao <weiqiang.hao@mediatek.com>
2022-08-24 14:48:56 +0800

    [ALPS07509451] ccci: add suspend/resume no_irq procedure
    Add suspend/resume no_irq procedure for dpmaif driver code.
    
    MTK-Commit-Id: af23fdbf6e87b9405727b0dfcdad8127de292ba4
    
    Signed-off-by: Weiqiang Hao <weiqiang.hao@mediatek.com>
    CR-Id: ALPS07509451
    Feature: CCCI_HIF_DATA
    Change-Id: If35360cf1d90769a1644d4970691c9855070facc

--- drivers/misc/mediatek/eccci/mt6785/md_sys1_platform.c
+++ drivers/misc/mediatek/eccci/mt6785/md_sys1_platform.c
@@ -52,6 +52,7 @@
 #include "ap_md_reg_dump.h"
 #endif
 #include "modem_secure_base.h"
+#include "hif/ccci_hif_dpmaif.h"
 
 static struct ccci_clk_node clk_table[] = {
 	{ NULL,	"scp-sys-md1-main"},
@@ -942,5 +943,15 @@ void ccci_notify_set_scpmem(void)
 			__func__, scp_smem_addr, size, key);
 	} else
 		CCCI_ERROR_LOG(MD_SYS1, TAG, "%s get_smem fail\n", __func__);
+}
+
+int ccci_modem_suspend_noirq(struct device *dev)
+{
+	return dpmaif_suspend_noirq(dev);
+}
+
+int ccci_modem_resume_noirq(struct device *dev)
+{
+	return dpmaif_resume_noirq(dev);
 }
 
