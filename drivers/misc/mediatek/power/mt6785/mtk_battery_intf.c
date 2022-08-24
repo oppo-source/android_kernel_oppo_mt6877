0c28f96d7f6a486ba82a4f51e5bc7e3bb618e459
mufaddal.ali <mufaddal.ali@mediatek.com>
2022-08-24 14:48:48 +0800

    [ALPS07501379] battery:fix coverity
    Return UI Soc from boot mode
    
    MTK-Commit-Id: 23ea06fe7963fbd34f927cf8a5a01f786c07888f
    
    Change-Id: Iebe04d0f871990439af9e58d7e4451e164b1fee6
    CR-Id: ALPS07501379
    Feature: Fuel Gauge
    Signed-off-by: mufaddal.ali <mufaddal.ali@mediatek.com>

--- drivers/misc/mediatek/power/mt6785/mtk_battery_intf.c
+++ drivers/misc/mediatek/power/mt6785/mtk_battery_intf.c
@@ -37,15 +37,22 @@ signed int battery_get_soc(void)
 
 signed int battery_get_uisoc(void)
 {
-	int boot_mode = 0; //phase out api: get_boot_mode();
+	struct mtk_battery *gm = get_mtk_battery();
 
-	if ((boot_mode == META_BOOT) ||
-		(boot_mode == ADVMETA_BOOT) ||
-		(boot_mode == FACTORY_BOOT) ||
-		(boot_mode == ATE_FACTORY_BOOT))
-		return 75;
+	if (gm != NULL) {
+		int boot_mode = gm->boot_mode;
+
+		if ((boot_mode == META_BOOT) ||
+			(boot_mode == ADVMETA_BOOT) ||
+			(boot_mode == FACTORY_BOOT) ||
+			(boot_mode == ATE_FACTORY_BOOT))
+			return 75;
+		else if (boot_mode == 0)
+			return gm->ui_soc;
+	}
 
 	return 50;
+
 }
 
 signed int battery_get_bat_temperature(void)
