33829ef6ab8caff919583a2902f0c1290f014231
lokesh.tripathi <lokesh.tripathi@mediatek.com>
2022-09-22 11:56:56 +0800

    [ALPS07570899] SCP: scp dvfs enable
    Handle mainpll disable for MT6785
    
    MTK-Commit-Id: 2a9e8d23f3d380a021594d0c33d53bede688d653
    
    Change-Id: If47da20c4894b9f202b98b7b530c884e51a26bf4
    Signed-off-by: lokesh.tripathi <lokesh.tripathi@mediatek.com>
    CR-Id: ALPS07570899
    Feature: SCP DVFS Kernel

--- drivers/misc/mediatek/scp/cm4/scp_dvfs.h
+++ drivers/misc/mediatek/scp/cm4/scp_dvfs.h
@@ -20,7 +20,13 @@
 #define DVFS_STATUS_CMD_DISABLE	(-8)
 
 #define CLK_26M					(26)
+
+#if defined(CONFIG_MACH_MT6785)
+#define MAINPLL_273M			(218)
+#else
 #define MAINPLL_273M			(273)
+#endif
+
 #define UNIVPLL_416M			(416)
 
 enum scp_state_enum {
@@ -39,7 +45,7 @@ enum {
 	CLK_HIGH_IRQ_EN_BIT = 17,
 };
 
-#if (defined(CONFIG_MACH_MT6781) || defined(CONFIG_MACH_MT6785))
+#if defined(CONFIG_MACH_MT6781)
 #include <linux/arm-smccc.h>
 enum scp_req_r {
 	SCP_REQ_RELEASE = 0,
